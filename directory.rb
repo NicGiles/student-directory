68283dffacaf761b8a66a7647dfceea379cc73fa
NicGiles <n.r.giles1@gmail.com>
2018-08-29 12:32:01 +0100

    refactored directory

--- directory.rb
+++ directory.rb
@@ -1,20 +1,26 @@
+
+#print the list of students
+students = [
+"Dr. Hannibal Lecter",
+"Darth Vader",
+"Nurse Ratched",
+"Michael Corleone",
+"Alex DeLarge",
+"The Wicked Witch of the West",
+"Terminator",
+"Freddy Kruger",
+"The Joker",
+"Joffrey Baratheon",
+"Norman Bates"
+]
+
+#and then print them
+
 puts "The students of the boss academy"
 puts "               "
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Kruger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
+students.each do |student|
+  puts student
+end
 
-# finally, we print the total number of students
-print "Overall, we have "
-#it’s important that print() doesn’t add new line characters
-print 11
-puts " great students"
-	
+#finally we print the total
+puts "Overall, we have #{students.count} great students"
